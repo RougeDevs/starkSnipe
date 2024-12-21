fc4ac90367808f571807e1481f93ff0acc3e2c44
0xShisuiii <arijeetg125@gmail.com>
2024-12-21 15:38:51 +0530

    telegram bot fixes

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,88 @@
 use std::time::Duration;
+use dotenv::dotenv;
 
 use provider::{Monitor, StarknetProviderError, StarknetProviderOptions};
-use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::{header::{HeaderMap, HeaderValue}, Error as ReqwestError};
 use starknet::core::types::{BlockId, EventFilter, Felt};
+use telegram::TelegramBot;
 use tokio::runtime::Builder;
 use url::Url;
 
 mod provider;
 mod telegram;
 
-fn main() -> Result<(), StarknetProviderError> {
+#[derive(Debug)]
+enum AppError {
+    Provider(StarknetProviderError),
+    Reqwest(ReqwestError),
+    Telegram(String),
+    Url(url::ParseError),
+    Other(String),
+}
+
+// Implement conversions from specific errors to AppError
+impl From<StarknetProviderError> for AppError {
+    fn from(err: StarknetProviderError) -> Self {
+        AppError::Provider(err)
+    }
+}
+
+impl From<ReqwestError> for AppError {
+    fn from(err: ReqwestError) -> Self {
+        AppError::Reqwest(err)
+    }
+}
+
+
+async fn run_monitor() -> Result<(), AppError> {
+    let url = Url::parse("https://starknet-mainnet.infura.io/v3/edd0fd50d7d948d58c513f38e5622da2").unwrap();
+    let mut headers = HeaderMap::new();
+    let hex_address = "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
+    let address = Felt::from_hex(hex_address).unwrap();
+    
+    headers.insert(
+        "Authorization", 
+        HeaderValue::from_static("edd0fd50d7d948d58c513f38e5622da2")
+    );
+    
+    let options = StarknetProviderOptions {
+        timeout: Duration::from_secs(30),
+        headers
+    };
+    
+    let listener = Monitor::new(url, options).expect("Failed to start monitor");
+    // let transfer_selector = get_selector_from_name("Transfer").unwrap();
+    
+    let filter = EventFilter {
+        from_block: Some(BlockId::Number(1100)),
+        to_block: None,
+        address: Some(address),
+        keys: Some(vec![]),
+    };
+
+    listener.listen_for_events(filter, |events| {
+        for event in events.events {
+            println!("New event received:");
+            println!("  From address: {:?}", event.from_address);
+            println!("  Block number: {:?}", event.block_number);
+            println!("  Transaction hash: {:?}", event.transaction_hash);
+            println!("  Data: {:?}", event.data);
+        }
+    }).await?;
+
+    Ok(())
+}
+
+fn main() -> Result<(),AppError> {
+    dotenv().ok();
     let rt = Builder::new_multi_thread().enable_all().build().unwrap();
+    // rt.block_on(run_monitor())?;
+
+    // Trigger Telegram bot
     rt.block_on(async {
-        let url = Url::parse("https://starknet-mainnet.infura.io/v3/edd0fd50d7d948d58c513f38e5622da2").unwrap();
-        let mut headers = HeaderMap::new();
-        let hex_address = "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
-        let address = Felt::from_hex(hex_address).unwrap();
-        
-        headers.insert(
-            "Authorization", 
-            HeaderValue::from_static("edd0fd50d7d948d58c513f38e5622da2")
-        );
-        
-        let options = StarknetProviderOptions {
-            timeout: Duration::from_secs(30),
-            headers
-        };
-        
-        let listener = Monitor::new(url, options).expect("Failed to start monitor");
-        // let transfer_selector = get_selector_from_name("Transfer").unwrap();
-        
-        let filter = EventFilter {
-            from_block: Some(BlockId::Number(1100)),
-            to_block: None,
-            address: Some(address),
-            keys: Some(vec![]),
-        };
-
-        listener.listen_for_events(filter, |events| {
-            for event in events.events {
-                println!("New event received:");
-                println!("  From address: {:?}", event.from_address);
-                println!("  Block number: {:?}", event.block_number);
-                println!("  Transaction hash: {:?}", event.transaction_hash);
-                println!("  Data: {:?}", event.data);
-            }
-        }).await?;
-
+        let bot = TelegramBot::new().map_err(|e| AppError::Telegram(format!("Failed to initialise telegram bot: {}", e))).unwrap();
+        let text = "This is a new chat test";
+        bot.send_message_with_buttons(7257416467, text, None).await?;
         Ok(())
     })
 }
--- src/telegram/mod.rs
+++ src/telegram/mod.rs
@@ -1,14 +1,31 @@
-use std::{env, fmt::format};
 use serde::{Deserialize, Serialize};
-
 use reqwest::{Client, Error};
-
 #[derive(Debug, Serialize)]
 struct SendMessageRequest {
     chat_id: i64,
     text: String,
     #[serde(skip_serializing_if = "Option::is_none")]
+    reply_markup: Option<InlineKeyboardMarkup>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     reply_to_message_id: Option<i64>,
+}
+
+#[derive(Debug, Serialize)]
+struct InlineKeyboardMarkup {
+    inline_keyboard: Vec<Vec<InlineKeyboardButton>>,
+}
+
+#[derive(Debug, Serialize)]
+struct InlineKeyboardButton {
+    text: String,
+    kind: InlineKeyboardButtonKind,
+}
+
+#[derive(Debug, Serialize)]
+enum InlineKeyboardButtonKind {
+    Callback(String),
+    Url(String),
+    // Add other variants as needed
 }
 
 pub struct TelegramBot {
@@ -19,7 +36,7 @@ pub struct TelegramBot {
 
 impl  TelegramBot {
     pub fn new() -> Result<Self, Error> {
-        let token = env::var("TELEGRAM_TOKEN").expect("TELEGRAM_TOKEN not found");
+        let token = std::env::var("TELEGRAM_TOKEN").expect("TELEGRAM_TOKEN not found");
         let client = Client::new();
         let base_url = format!("https://api.telegram.org/bot{}", token);
 
@@ -36,10 +53,44 @@ impl  TelegramBot {
         let message = SendMessageRequest {
             chat_id,
             text: text.to_string(),
+            reply_markup: None,
+            reply_to_message_id: reply_to
+        };
+        let json_body = serde_json::to_string(&message)
+            .expect("Failed to serialize message");
+        let response = self.client.post(&url).json(&json_body).send().await?;
+        if !response.status().is_success() {
+            println!("Error sending message: {:?}", response.text().await?);
+        }
+
+        Ok(())
+    }
+
+    pub async fn send_message_with_buttons(&self, chat_id: i64, text: &str, reply_to: Option<i64>) -> Result<(), Error> {
+        let url = format!("{}/sendMessage", self.base_url);
+
+        let keyboard = InlineKeyboardMarkup {
+            inline_keyboard: vec![vec![
+                InlineKeyboardButton {
+                    text: "Buy".to_string(),
+                    kind: InlineKeyboardButtonKind::Callback("action:buy".to_string())
+                },
+                InlineKeyboardButton {
+                    text: "Sell".to_string(),
+                    kind: InlineKeyboardButtonKind::Callback("action:sell".to_string()),
+                }
+            ]]
+        };
+
+        let message = SendMessageRequest {
+            chat_id,
+            text: text.to_string(),
+            reply_markup: Some(keyboard),
             reply_to_message_id: reply_to
         };
 
-        let response = self.client.post(&url).body(&message).send().await?;
+        let json_body = serde_json::to_string(&message).expect("Failed to serialize message");
+        let response = self.client.post(&url).json(&json_body).send().await?;
         if !response.status().is_success() {
             println!("Error sending message: {:?}", response.text().await?);
         }
